aecca1085eff483d3ef33bfaf7ecea919e7a7337
chetsadaphon <114697178+kihwchetsada@users.noreply.github.com>
2024-09-23 11:16:03 +0700

    Create main_Screen.dart

---
+++ main_Screen.dart
@@ -0,0 +1,13 @@
+import 'package:basic2/page/FirstScreen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+     return MaterialApp(home: Firstscreen());
+  }
+}
